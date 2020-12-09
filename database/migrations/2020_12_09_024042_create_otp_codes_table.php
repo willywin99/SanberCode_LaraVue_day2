1705a565a079a0789f31fc04e2cc1582498aca72
willywin99 <willywinsianturi@gmail.com>
2020-12-09 10:33:50 +0700

    Second Commit

---
+++ database/migrations/2020_12_09_024042_create_otp_codes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otp_codes', function (Blueprint $table) {
+            // $table->bigIncrements('id');
+            $table->uuid('id')->primary();
+            $table->integer('otp_code');
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->dateTime('expire_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otp_codes');
+    }
+}
